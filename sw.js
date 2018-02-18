c38f9763f3adfffc6e231a20aa8ac1212efed99b
Claire Parker <claireparker@users.noreply.github.com>
2018-02-18 11:24:19 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/magento-gatsby-blog/app-d100b9f604b201f08a69.js","4665774b5bfb92af4c76183e891c82fd"],["/magento-gatsby-blog/commons-9d85a4b72242c55d8f4f.js","4c251ee3be5d8a154eb64863ad5ffb09"],["/magento-gatsby-blog/index.html","2f6da441d5b72375906f3ce6cd1c301e"],["/magento-gatsby-blog/offline-plugin-app-shell-fallback/index.html","a197a76580647a0bc3d515d5674077b5"]];
+var precacheConfig = [["/app-d100b9f604b201f08a69.js","ad39da69c7464909f68208b27786fefe"],["/commons-9d85a4b72242c55d8f4f.js","f407fbdc7ecdf8eebf9fa12f2b88a10a"],["/index.html","ede56dc8e5584e25f9b03e20d1078be3"],["/offline-plugin-app-shell-fallback/index.html","bbd8b714ca2c0878be3ac8f0bb6a12a7"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
